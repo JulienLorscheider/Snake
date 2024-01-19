0b00ac244a2cbf1d6f5333e11bf5e3d42f8ff2f0
LORSCHEIDER Julien <julien.lorscheider8@etu.univ-lorraine.fr>
2024-01-19 10:27:26 +0100

    Initial Commit

---
+++ Snake/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  Snake
+//
+//  Created by Julien on 19/01/2024.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
